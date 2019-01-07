3b1e68961e88c5a3ae2ae36db670f5f8567c1986
daniarmes <daniarmes@gmail.com>
2019-01-07 23:35:08 +0100

    Login View Model (binding)

---
+++ Login/ViewModels/LoginViewModel.cs
@@ -0,0 +1,48 @@
+﻿namespace Login.ViewModels
+{
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsRunning 
+        {
+            get;
+            set;
+        }
+        public bool IsRemember
+        {
+            get;
+            set;
+        }
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+        public ICommand LoginFacebookCommand
+        {
+            get;
+            set;
+        }
+        public ICommand LoginGmailCommand
+        {
+            get;
+            set;
+        }
+
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+        }
+    }
+}
